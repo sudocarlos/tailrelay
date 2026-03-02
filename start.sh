42f8a9bb3740e56146bcd959c5c3c39a02d50405
sudocarlos <me@sudocarlos.com>
2026-03-02 07:00:04 -0500

    chore(release): bump version to v0.6.0

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 trap 'shutdown' TERM INT
-TAILRELAY_VERSION=v0.5.0
+TAILRELAY_VERSION=v0.6.0
 
 # Accept a single comma‑separated list of port:target pairs
 # Each item in the list represents one socat relay
