467eb79b3af52b659a2191c4c82ee38ee6574153
sudocarlos <me@sudocarlos.com>
2026-03-28 08:56:54 -0400

    chore: release v0.8.4
    - bump tailscale from v1.92.5 to v1.96.4
    - update version in start.sh to v0.8.4
    - add changelog entry for v0.8.4

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 trap 'shutdown' TERM INT
-TAILRELAY_VERSION=v0.8.3
+TAILRELAY_VERSION=v0.8.4
 
 # Accept a single comma‑separated list of port:target pairs
 # Each item in the list represents one socat relay
